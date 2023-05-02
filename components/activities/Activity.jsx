3e5143121679e3477b53e02d811da931bdd1aa2b
alekskimeu <kim3ual3x@gmail.com>
2023-05-02 20:11:36 +0300

    Implemented mobile responsiveness on activities page

--- components/activities/Activity.jsx
+++ components/activities/Activity.jsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 
 export default function Activity({image, title, description}) {
     return (
-        <div className="grid grid-cols-2 items-center">
+        <div className="grid grid-cols-1 gap-5 mb-10 lg:mb-0 lg:grid-cols-2 lg:gap-0 items-center">
+            <Image src={image} className="rounded-lg lg:rounded-none" />
             <div className="content">
              <h1 className="font-semibold text-2xl mb-5">{title}</h1>
              <p className="">{description}</p>
             </div>
-            <Image src={image} />
         </div>
     )
 }

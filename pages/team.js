2d846123c3d4b3861aedbf23db0c02f7d76547f7
alekskimeu <kim3ual3x@gmail.com>
2023-05-02 19:48:51 +0300

    Implemented mobile responsiveness on teams page

--- pages/team.js
+++ pages/team.js
@@ -7,57 +7,137 @@ export default function Team() {
     <Layout>
       <section className="pt-[3rem]">
         <div className="container mx-auto">
-            <div className="border-b pb-3">
-                <h1 className="uppercase text-xl font-bold">Goalkeepers</h1>
-            </div>
-          <div className="grid grid-cols-4 gap-[2rem] mt-[2rem]">
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
+          <div className="border-b pb-3">
+            <h1 className="uppercase text-xl font-bold">Goalkeepers</h1>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[2rem] mt-[2rem]">
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
           </div>
         </div>
       </section>
 
       <section className="pt-[3rem]">
         <div className="container mx-auto">
-            <div className="border-b pb-3">
-                <h1 className="uppercase text-xl font-bold">Defenders</h1>
-            </div>
+          <div className="border-b pb-3">
+            <h1 className="uppercase text-xl font-bold">Defenders</h1>
+          </div>
 
-          <div className="grid grid-cols-4 gap-[2rem] mt-[2rem]">
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[2rem] mt-[2rem]">
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
           </div>
         </div>
       </section>
 
       <section className="pt-[3rem]">
         <div className="container mx-auto">
-            <div className="border-b pb-3">
-                <h1 className="uppercase text-xl font-bold">Midfielders</h1>
-            </div>
-          <div className="grid grid-cols-4 gap-[2rem] mt-[2rem]">
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-            <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
+          <div className="border-b pb-3">
+            <h1 className="uppercase text-xl font-bold">Midfielders</h1>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[2rem] mt-[2rem]">
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
           </div>
         </div>
       </section>
       <section className="pt-[3rem]">
         <div className="container mx-auto">
-            <div className="border-b pb-3">
-                <h1 className="uppercase text-xl font-bold">Forwards</h1>
-            </div>
-            <div className="grid grid-cols-4 gap-[2rem] mt-[2rem]">
-                <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-                <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-                <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-                <PlayerCard image={image} number={1} firstName="John" lastName="Doe" />
-            </div>
+          <div className="border-b pb-3">
+            <h1 className="uppercase text-xl font-bold">Forwards</h1>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[2rem] mt-[2rem]">
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+            <PlayerCard
+              image={image}
+              number={1}
+              firstName="John"
+              lastName="Doe"
+            />
+          </div>
         </div>
       </section>
     </Layout>

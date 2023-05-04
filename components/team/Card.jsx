a50d2ed43bd6233bb39044d8bd48d557f47706cd
alekskimeu <kim3ual3x@gmail.com>
2023-05-04 14:23:44 +0300

    Styled player card

--- components/team/Card.jsx
+++ components/team/Card.jsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 const Card = ({ number, firstName, lastName, image }) => {
   return (
-    <Link href="" className="relative border rounded-xl bg-[#2b2b2b]">
-      <Image src={image} className="w-[100%]" />
+    <Link href="" className="group relative border rounded-xl player-card overflow-hidden">
+      <Image src={image} className="w-[100%] grayscale group-hover:grayscale-0 transition group-hover:scale-105 duration-1000" />
       <div className="absolute bottom-[1rem] left-[1rem] flex flex-col gap-[.1rem] z-10">
-        <span className="font-semibold text-xl border-b-2 border-[#17403F] w-fit text-white">
+        <span className="font-semibold text-md md:text-lg border-b-2 border-[#17403F] w-fit text-white">
           {number}
         </span>
-        <span className="font-bold text-2xl text-white uppercase">{firstName}</span>
-        <span className="font-bold text-2xl text-white uppercase">{lastName}</span>
+        <span className="font-bold text-lg md:text-xl text-white uppercase">{firstName}</span>
+        <span className="font-bold text-lg md:text-xl text-white uppercase">{lastName}</span>
       </div>
     </Link>
   );

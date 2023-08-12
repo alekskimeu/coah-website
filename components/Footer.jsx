a21fc424a27c06c8a5160fd8e15858bd773827e1
alekskimeu <kim3ual3x@gmail.com>
2023-08-12 14:18:13 +0300

    Modified menu items

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,21 +5,27 @@ const Footer = () => {
     <footer className="h-[7rem] lg:h-[5rem] bg-[#283891] flex items-center py-4 px-[2rem] absolute bottom-0" style={{width: "100%"}}>
       <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center gap-5 text-white">
         <div className="flex items-center gap-5">
-          <Link href="/activities" className="opacity-80">
+        <Link href="/" className="opacity-80 text-sm">
+            Home
+          </Link>
+          <Link href="/about" className="opacity-80 text-sm">
+            About
+          </Link>
+          <Link href="/activities" className="opacity-80 text-sm">
             Activities
           </Link>
-          <Link href="/team" className="opacity-80">
-            Team
+          <Link href="/events" className="opacity-80 text-sm">
+            Events
           </Link>
-          <Link href="/blog" className="opacity-80">
+          <Link href="/blog" className="opacity-80 text-sm">
             Blog
           </Link>
-          <Link href="/newsletters" className="opacity-80">
+          <Link href="/newsletters" className="opacity-80 text-sm">
             Newsletters
           </Link>
         </div>
-        <span className="opacity-80 text-sm">
-          &copy; {new Date().getFullYear()} COAH
+        <span className="opacity-80 text-xs">
+          &copy; COAH {new Date().getFullYear()} - All Rights Reserved
         </span>
       </div>
     </footer>

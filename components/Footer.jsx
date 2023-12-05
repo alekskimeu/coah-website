5dfccf24e11344aed1dd1e04f7b56e7258b5493c
alekskimeu <kim3ual3x@gmail.com>
2023-12-05 22:30:35 +0300

    Fixed footer links layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="h-[7rem] lg:h-[5rem] bg-[#283891] flex items-center py-4 px-[2rem] absolute bottom-0" style={{width: "100%"}}>
+    <footer
+      className="h-[7rem] lg:h-[5rem] bg-[#283891] flex items-center py-4 px-[2rem] absolute bottom-0"
+      style={{ width: "100%" }}
+    >
       <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center gap-5 text-white">
-        <div className="flex items-center gap-5">
-        <Link href="/" className="opacity-80 text-sm">
+        <div className="flex flex-wrap justify-center items-center gap-2 md:gap-5 px-1">
+          <Link href="/" className="opacity-80 text-sm">
             Home
           </Link>
           <Link href="/about" className="opacity-80 text-sm">
